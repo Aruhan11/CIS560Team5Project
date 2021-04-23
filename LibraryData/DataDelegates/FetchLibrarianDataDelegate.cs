8963729fd2005b0a8d9a8b3b51308889eb1cc287
austinberry1 <amberry@ksu.edu>
2021-04-23 05:55:50 -0500

    All DataDelegates Added 4/43/2021

---
+++ LibraryData/DataDelegates/FetchLibrarianDataDelegate.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess;
+using LibarayData.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+
+namespace LibarayData.DataDelegates
+{
+
+    internal class FetchLibrarianDataDelegate : DataReaderDelegate<Librarian>
+    {
+
+        private readonly int librarianID;
+
+        public FetchLibrarianDataDelegate(int librarianID)
+           : base("Library.FetchLibrarian")
+        {
+            this.librarianID = librarianID;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            var p = command.Parameters.Add("UserID", SqlDbType.Int);
+            p.Value = librarianID;
+        }
+
+        public override Librarian Translate(SqlCommand command, IDataRowReader reader)
+        {
+            if (!reader.Read())
+                throw new RecordNotFoundException(librarianID.ToString());
+
+            return new Librarian(librarianID,
+               reader.GetInt32("UserID"));
+        }
+
+    }
+}
