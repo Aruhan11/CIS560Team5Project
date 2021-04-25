8c18a20f67815f4d0771794930578c15ab3dbd6d
Lauren Grieb <beverly220@ksu.edu>
2021-04-24 23:38:12 -0500

    fjdklsjflds

---
+++ LibraryData/DataDelegates/RankingCreatorByUserDelegate.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess;
+using LibarayData.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+
+namespace LibarayData.DataDelegates
+{
+    internal class RankingCreatorByUserDelegate : DataReaderDelegate<IReadOnlyList<CreatorByUser>>
+    {
+        private readonly int userId;
+
+        public RankingAssetByUserDelegate(int userId)
+           : base("Library.RankingCreatorByUser")
+        {
+            this.userId = userId;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            command.Parameters.AddWithValue("UserID", userId);
+        }
+
+        public override IReadOnlyList<CreatorByUser> Translate(SqlCommand command, IDataRowReader reader)
+        {
+            var creatorList = new List<CreatorByUser>();
+
+            while (reader.Read())
+            {
+                creatorList.Add(new CreatorByUser(
+                    reader.GetInt32("RowNumber"),
+                    reader.GetString("UserName"),
+                    reader.GetString("AssetCountRank"),
+                    reader.GetString("CreatorName"),
+                    reader.GetInt32("CompanyName"),
+                    reader.GetString("AssetName")))
+            }
+
+            return creatorList;
+        }
+    }
+}
